0a9f7cfe9973899078484073d5fb6c6826d25e9e
Joao <jvcmv.jvcmarcenes@gmail.com>
2021-11-24 15:42:20 -0300

    improved std math library

--- src/interpreter/globals/math.rs
+++ src/interpreter/globals/math.rs
@@ -79,6 +79,145 @@ fn sqrt() -> Value {
 	Value::Callable(new_rcref(Sqrt))
 }
 
+fn floor() -> Value {
+	#[derive(Clone, Debug)] struct Floor;
+	
+	impl Callable for Floor {
+		fn arity(&self) -> usize { 1 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (val, pos) = args[0].clone();
+			Value::Num(val.to_num(pos)?.floor()).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Floor))
+}
+
+fn ceil() -> Value {
+	#[derive(Clone, Debug)] struct Ceil;
+	
+	impl Callable for Ceil {
+		fn arity(&self) -> usize { 1 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (val, pos) = args[0].clone();
+			Value::Num(val.to_num(pos)?.ceil()).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Ceil))
+}
+
+fn round() -> Value {
+	#[derive(Clone, Debug)] struct Round;
+	
+	impl Callable for Round {
+		fn arity(&self) -> usize { 1 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (val, pos) = args[0].clone();
+			Value::Num(val.to_num(pos)?.round()).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Round))
+}
+
+fn abs() -> Value {
+	#[derive(Clone, Debug)] struct Abs;
+	
+	impl Callable for Abs {
+		fn arity(&self) -> usize { 1 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (val, pos) = args[0].clone();
+			Value::Num(val.to_num(pos)?.abs()).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Abs))
+}
+
+fn max() -> Value {
+	#[derive(Clone, Debug)] struct Max;
+	
+	impl Callable for Max {
+		fn arity(&self) -> usize { 2 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (v0, p0) = args[0].clone();
+			let (v1, p1) = args[1].clone();
+			Value::Num(v0.to_num(p0)?.max(v1.to_num(p1)?)).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Max))
+}
+
+fn min() -> Value {
+	#[derive(Clone, Debug)] struct Floor;
+	
+	impl Callable for Floor {
+		fn arity(&self) -> usize { 2 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (v0, p0) = args[0].clone();
+			let (v1, p1) = args[1].clone();
+			Value::Num(v0.to_num(p0)?.min(v1.to_num(p1)?)).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Floor))
+}
+
+fn clamp() -> Value {
+	#[derive(Clone, Debug)] struct Clamp;
+	
+	impl Callable for Clamp {
+		fn arity(&self) -> usize { 3 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (v0, p0) = args[0].clone();
+			let (v1, p1) = args[1].clone();
+			let (v2, p2) = args[2].clone();
+			Value::Num(v0.to_num(p0)?.clamp(v1.to_num(p1)?, v2.to_num(p2)?)).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Clamp))
+}
+
+fn frac() -> Value {
+	#[derive(Clone, Debug)] struct Frac;
+	
+	impl Callable for Frac {
+		fn arity(&self) -> usize { 1 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (val, pos) = args[0].clone();
+			Value::Num(val.to_num(pos)?.fract()).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Frac))
+}
+
+fn sign() -> Value {
+	#[derive(Clone, Debug)] struct Sign;
+	
+	impl Callable for Sign {
+		fn arity(&self) -> usize { 1 }
+	
+		fn call(&mut self, _pos: SourcePos, _interpreter: &mut Interpreter, args: Vec<(Value, SourcePos)>) -> Result<Value> {
+			let (v0, p0) = args[0].clone();
+			Value::Num(v0.to_num(p0)?.signum()).wrap()
+		}
+	}
+	
+	Value::Callable(new_rcref(Sign))
+}
+
 pub fn math() -> Value {
 	let mut map = HashMap::new();
 
@@ -88,7 +227,17 @@ pub fn math() -> Value {
 		("tg", tan()),
 		("pow", pow()),
 		("sqrt", sqrt()),
-		("pi", Value::Num(3.141592653589793238462643383279502884197139699)),
+		("floor", floor()),
+		("ceil", ceil()),
+		("round", round()),
+		("abs", abs()),
+		("max", max()),
+		("min", min()),
+		("clamp", clamp()),
+		("sign", sign()),
+		("frac", frac()),
+		("pi", Value::Num(std::f64::consts::PI)),
+		("e", Value::Num(std::f64::consts::E)),
 	];
 
 	for (key, val) in v {
@@ -96,5 +245,4 @@ pub fn math() -> Value {
 	}
 
 	Value::Object(map)
-	
 }
